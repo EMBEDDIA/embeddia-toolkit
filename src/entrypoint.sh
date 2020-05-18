379b0d01d4802d2016af5518c4adeadbef357c83
rsirel <rsirel@texta.ee>
2020-05-18 09:31:18 +0300

    update entrypoint

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -4,6 +4,4 @@ source activate embeddia-rest && python /var/embeddia-rest/src/manage.py migrate
 source activate embeddia-rest && python /var/embeddia-rest/src/manage.py collectstatic --noinput --clear
 chown www-data:www-data -R /var/embeddia-rest/static/ && chmod 777 -R /var/embeddia-rest/static/
 
-set | egrep "^(DJANGO|TEXTA|PYTHON|LC_|LANG)" | sed -e 's/^/export /' > .env
-
 exec "$@"
